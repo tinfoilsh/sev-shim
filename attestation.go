33b5ac071787cc9a26303f4d91b89e98911fe697
Nate Sales <nate@natesales.net>
2025-06-03 13:52:37 -0700

    feat: TDX attestation

--- attestation.go
+++ attestation.go
@@ -4,17 +4,20 @@ import (
 	"encoding/base64"
 	"fmt"
 
-	"github.com/google/go-sev-guest/abi"
-	"github.com/google/go-sev-guest/client"
+	sevabi "github.com/google/go-sev-guest/abi"
+	sevclient "github.com/google/go-sev-guest/client"
+	tdxclient "github.com/google/go-tdx-guest/client"
+	"github.com/klauspost/cpuid/v2"
+	log "github.com/sirupsen/logrus"
 	"github.com/tinfoilsh/verifier/attestation"
 )
 
-// attestationReport gets a SEV-SNP signed attestation report over a TLS certificate fingerprint
-func attestationReport(certFP string) (*attestation.Document, error) {
+// sevAttestationReport gets a SEV-SNP signed attestation report over a TLS certificate fingerprint
+func sevAttestationReport(certFP string) (*attestation.Document, error) {
 	var userData [64]byte
 	copy(userData[:], certFP)
 
-	qp, err := client.GetQuoteProvider()
+	qp, err := sevclient.GetQuoteProvider()
 	if err != nil {
 		return nil, fmt.Errorf("failed to get quote provider: %v", err)
 	}
@@ -23,12 +26,44 @@ func attestationReport(certFP string) (*attestation.Document, error) {
 		return nil, fmt.Errorf("failed to get quote: %v", err)
 	}
 
-	if len(report) > abi.ReportSize {
-		report = report[:abi.ReportSize]
+	if len(report) > sevabi.ReportSize {
+		report = report[:sevabi.ReportSize]
 	}
 
 	return &attestation.Document{
 		Format: attestation.SevGuestV1,
 		Body:   base64.StdEncoding.EncodeToString(report),
 	}, nil
+}
+
+// tdxAttestationReport gets a TDX signed attestation report over a TLS certificate fingerprint
+func tdxAttestationReport(certFP string) (*attestation.Document, error) {
+	var userData [64]byte
+	copy(userData[:], certFP)
+
+	qp, err := tdxclient.GetQuoteProvider()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get quote provider: %v", err)
+	}
+	report, err := qp.GetRawQuote(userData)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get quote: %v", err)
+	}
+
+	return &attestation.Document{
+		Format: "https://tinfoil.sh/predicate/tdx-guest/v1",
+		Body:   base64.StdEncoding.EncodeToString(report),
+	}, nil
+}
+
+func attestationReport(certFP string) (*attestation.Document, error) {
+	if cpuid.CPU.IsVendor(cpuid.AMD) {
+		log.Info("Requesting AMD SEV-SNP quote")
+		return sevAttestationReport(certFP)
+	} else if cpuid.CPU.IsVendor(cpuid.Intel) {
+		log.Info("Requesting Intel TDX quote")
+		return tdxAttestationReport(certFP)
+	} else {
+		return nil, fmt.Errorf("attestation report for vendor %s not supported", cpuid.CPU.VendorString)
+	}
 }
